e8649bc73115b2f01b46902f018ce857c82f5ad8
ecocaval <ecocaval@hotmail.com>
2022-10-18 04:07:46 -0300

    working starting function

--- script.js
+++ script.js
@@ -12,9 +12,7 @@ function askCardsQuantity() {
 
 function sortCards() {
     const cards = Array.from(document.querySelectorAll('.card'));
-    
-    console.log(cards);
-    
+        
     let cards_classes = [];
 
     for(let card_index in cards) {
@@ -31,18 +29,7 @@ function shuffle_cards(cards, cards_classes) {
         return 0.5 - Math.random();
     })
 
-    change_cards_classes(cards, classes_shuffled);
+    console.log(classes_shuffled);
 }
 
-function change_cards_classes(cards, classes_shuffled) {
-    for(let card in cards) {
-        const card_class = card + 1;
-        cards[card].classList.remove(`${card_class}`);
-    }
 
-    for(let card in cards) {
-        cards[card].classList.add(classes_shuffled[card])
-    }
-
-    console.log(cards);
-}
