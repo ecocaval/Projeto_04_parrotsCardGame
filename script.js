ca033fc63134858edf7aced813623ec1e2c4ee7e
ecocaval <ecocaval@hotmail.com>
2022-10-19 03:07:13 -0300

    working functions for detecting matches

--- script.js
+++ script.js
@@ -1,35 +1,97 @@
 /**
  * @brief Parrot Game - 17/10/22
- */
+*/ 
 
-function askCardsQuantity() {
-    let cardsQuantity = 0;
-    while(cardsQuantity === 0 || (cardsQuantity % 2 === 1) || cardsQuantity < 4 || cardsQuantity > 14) {
-        cardsQuantity = prompt('Com quantas cartas voce deseja jogar');
-    }
-    sortCards();
-} 
+// num.sort((a,b) => {
+//     return 0.5 - Math.random();
+// })
 
-function sortCards() {
-    const cards = Array.from(document.querySelectorAll('.card'));
-        
-    let cards_classes = [];
+// firts function of the program
+askCardQuantity();
 
-    for(let card_index in cards) {
-        cards_classes[card_index] = cards[card_index].classList[2];
-    }
+function testCard(card) {
+    // regex user for extracting the gif name from the image file
+    const extractGifRegex = /images\/(.*).gif/
+    
+    let cardBack = card.childNodes[1];
+    let cardFront = card.childNodes[3];
 
-    shuffle_cards(cards, cards_classes);
+    // variable containing the classe name of the card clicked
+    const cardClass = extractGifRegex.exec(cardFront.childNodes[1].src)[1];
+
+    flipCard(cardBack, cardFront)
+
+    console.log(testForMatch(cardClass));
 }
 
-function shuffle_cards(cards, cards_classes) {
-    let classes_shuffled = cards_classes;
-
-    classes_shuffled.sort((a,b) => {
-        return 0.5 - Math.random();
-    })
-
-    console.log(classes_shuffled);
+function flipCard(cardBack, cardFront) {
+    cardFront.classList.toggle('hidden');
+    cardBack.classList.toggle('hidden');
 }
 
+function testForMatch(class_to_match) {
+
+    const extractGifRegex = /images\/(.*).gif/
+
+    const cardFront = Array.from(document.querySelectorAll('.cardFront'));
+
+    const cardsToTest = [];
+
+    for(let card in cardFront) {
+        const cardClass = extractGifRegex.exec(cardFront[card].childNodes[1].src)[1];
+        if(cardClass === class_to_match) {
+            cardsToTest.push(cardFront[card]);
+        }
+    }    
+    
+    let cardsHidden = 0;
+
+    for(let card in cardsToTest){
+        if(cardsToTest[card].classList.contains('hidden')) {
+            cardsHidden++
+        }
+    }
+
+    if(cardsHidden === 0) {
+        return true;
+    }
+    return false;
+}
+
+function askCardQuantity() {
+    let cardQuantity = 0;
+
+    while(cardQuantity < 4 || cardQuantity > 14 || (cardQuantity % 2 !== 0)) {
+        cardQuantity = prompt('Por favor, insira o número de cartas que deseja:');
+    }
+
+    createGameCards(cardQuantity);
+}
+
+function createGameCards(cardQuantity) {
+
+    let cardSection = document.querySelector('.cardSection');
+
+    let cardClasses = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 'tripletsparrot', 'unicornparrot'];    
+
+    cardClasses = doubleTheArray(cardClasses);
+
+    for(let contador = 0; contador < cardQuantity; contador++) {
+        cardSection.innerHTML += `
+        <div onclick="testCard(this)">
+            <div class="cardBack">
+                <img src="images/back.png">
+            </div>
+            <div class="cardFront hidden">
+                <img src="images/${cardClasses[contador]}.gif">
+            </div>
+        </div>
+        `;
+    }
+}
+
+function doubleTheArray(arrayToDouble) {
+    arrayToDouble = arrayToDouble.concat(arrayToDouble);
+    return arrayToDouble;
+}
 
