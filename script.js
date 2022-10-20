0a7cb39154a3b3087312d98c057ac4b9b90ffaed
ecocaval <ecocaval@hotmail.com>
2022-10-20 12:40:28 -0300

    organizing the functions order in the script.js

--- script.js
+++ script.js
@@ -9,214 +9,6 @@ let numberOfPlays = 0;
 // asks the quantity of cards to be displayed, so we can start the game
 askCardQuantity();
 
-function extractNameFromGifFile(cardFrontDiv) {
-
-    // regex user for extracting the gif name from the image file
-    const extractGifRegex = /images\/(.*).gif/;
-
-    // applies the regex above in the card front div received
-    const gifFileName = extractGifRegex.exec(cardFrontDiv.childNodes[1].src)[1];
-
-    return gifFileName;
-}
-
-// called everytime the user clicks card
-function testCard(card) {
-    
-    /* 
-        In this game every card has a div for it's front part and a div for 
-        it's back part, we must select both to create our flipping cards logic 
-    */
-    const cardsFront = Array.from(document.querySelectorAll('.cardFront'));
-    const cardsBack = Array.from(document.querySelectorAll('.cardBack'));
-
-    /*
-        childNodes[3] -> selects div.cardBack
-        childNodes[1] -> selects div.cardFront
-    */
-    let clickedCardBack = card.childNodes[3];
-    let clickedCardFront = card.childNodes[1];          
-    
-    // variable containing the classe name of the card clicked
-    const cardClass = extractNameFromGifFile(clickedCardFront);
-    
-    // extracts an array containing only the 2 cards with the class being tested
-    const cardsToMatch = extractCardsToMatch(cardsFront, cardClass);    
-
-    // the game does not flip a matched card
-    if(clickedCardBack.classList.contains('rotate') && !cardIsNotMatched(getFlippedCards(cardsFront,cardsBack)[0], clickedCardFront)) {
-        return;
-    }
-    
-    // flips the card clicked
-    flipCard(clickedCardBack, clickedCardFront);
-
-    // checks if the card clicked is matched
-    if(testForMatch(cardsToMatch)) {
-        numberOfPlays++;              
-        changeMatchedCardsColor(cardsFront);
-    } else {
-        flipBothCardsDisplayed(cardsFront, cardsBack);
-    }
-
-    // checks if the game is over, if it is displays the 'numberOfPlays' played
-    if(gameIsOver(cardsFront)) {
-        setTimeout(function() {
-            alert(`Você ganhou em ${numberOfPlays} jogadas!`);       
-        }, 500);
-    }
-}
-
-// adds pink background color to card when it's match is found
-function changeMatchedCardsColor(cardsFrontDiv) {
-    for(let cardCounter in cardsFrontDiv) {
-        if(!cardsFrontDiv[cardCounter].classList.contains('rotate')) {
-            cardsFrontDiv[cardCounter].classList.add('cardMatched');
-        }
-    }
-}
-
-// checks if all cards were flipped
-function gameIsOver(cardsFront) {
-    if(numberOfCardsHidden(cardsFront) === 0) {
-        return true;
-    }
-    return false;
-}
-
-// returns an matrix containing all the cards flipped front and back divs
-function getFlippedCards(allCardsFrontDiv, allCardsBackDiv){
-    // array containing all cards fliped front div
-    const cardsFlipedFrontDiv = [];
-    const cardsFlipedBackDiv = [];
-    
-    for(let cardCounter in allCardsFrontDiv) {
-        if(!allCardsFrontDiv[cardCounter].classList.contains('rotate')) {
-            cardsFlipedFrontDiv.push(allCardsFrontDiv[cardCounter]);
-            cardsFlipedBackDiv.push(allCardsBackDiv[cardCounter]);
-        }
-    }    
-    return [cardsFlipedFrontDiv, cardsFlipedBackDiv];
-}
-
-// if the 2 cards selected were not matched, the game must flips both cards back
-function flipBothCardsDisplayed(allCardsFront, allCardsBack) {
-    // arrays containing all cards fliped front and back div
-    const cardsFlipedFront = getFlippedCards(allCardsFront, allCardsBack)[0];
-    const cardsFlipedBack = getFlippedCards(allCardsFront, allCardsBack)[1];
-
-    let quantityOfCardsFliped = 0;
-
-    setTimeout(() => {
-        for(let cardCounter in cardsFlipedFront) {
-            if(cardIsNotMatched(cardsFlipedFront, cardsFlipedFront[cardCounter]) && !cardIsAlone(cardsFlipedFront)) {
-                flipCard(cardsFlipedFront[cardCounter], cardsFlipedBack[cardCounter]);
-                quantityOfCardsFliped++;
-            }
-        }  
-        // checks if the play was completed, so we can increment 'numberOfPlays'
-        if(quantityOfCardsFliped >= 2) {
-            numberOfPlays++;
-        }
-    }, 600);
-}
-
-// checks if just 1 card was selected, so we can mantain it being displayed
-function cardIsAlone(cardsFliped) {
-
-    // this variable will be decremented so we can disregard the cards that were already match
-    let quantityOfCardsFlipped = cardsFliped.length;    
-
-   // variable containing the classe name of the card clicked
-   const cardsToAnalyzeClasses = [];
-
-   for(let cards in cardsFliped) {
-       cardsToAnalyzeClasses.push(extractNameFromGifFile(cardsFliped[cards]));
-   }
-
-   for(let currentCard = 0; currentCard < cardsToAnalyzeClasses.length; currentCard++) {
-       for(let nextCard = currentCard + 1; nextCard < cardsToAnalyzeClasses.length; nextCard++) {
-           if(cardsToAnalyzeClasses[currentCard] === cardsToAnalyzeClasses[nextCard]) {
-            quantityOfCardsFlipped -= 2;
-           }
-       }
-   }
-
-   return (quantityOfCardsFlipped === 1);
-}
-
-function cardIsNotMatched(cardsFliped, cardToAnalyse) {
-
-     // regex user for extracting the gif name from the image file
-     const extractGifRegex = /images\/(.*).gif/;
-
-    // variable containing the classe name of the card clicked
-    const cardsToAnalyzeClasses = [];
-    const classesWithBothCardsFlipped = [];
-    const cardTestedClass = extractGifRegex.exec(cardToAnalyse.childNodes[1].src)[1];
-
-    for(let cards in cardsFliped) {
-        cardsToAnalyzeClasses.push(extractGifRegex.exec(cardsFliped[cards].childNodes[1].src)[1]);
-    }
-
-    for(let i = 0; i < cardsToAnalyzeClasses.length; i++) {
-        for(let j = i + 1; j < cardsToAnalyzeClasses.length; j++) {
-            if(cardsToAnalyzeClasses[i] === cardsToAnalyzeClasses[j]) {
-                classesWithBothCardsFlipped.push(cardsToAnalyzeClasses[i]);
-            }
-        }
-    }
-
-    if(classesWithBothCardsFlipped.includes(cardTestedClass)) {
-        return false;
-    }
-    return true;
-}
-
-// flips the card switching the '.rotate' class from both card "sub divs" (cardBack and cardFront)
-function flipCard(cardBack, cardFront) {
-    cardFront.classList.toggle('rotate');
-    cardBack.classList.toggle('rotate');
-}
-
-// checks if there was a match for the card selected
-function testForMatch(cardsToTest) {
-    if(numberOfCardsHidden(cardsToTest) === 0) {
-        return true;
-    }
-    return false;
-}
-
-// checks the quantity of hidden cards in a certain array, returns the number of cards hidden
-function numberOfCardsHidden(cardsToTest){
-    let cardsHidden = 0;
-
-    for(let card in cardsToTest){
-        if(cardsToTest[card].classList.contains('rotate')) {
-            cardsHidden++
-        }
-    }
-    return cardsHidden;
-}
-
-// extracts an array containing only the 2 cards with the class being tested
-function extractCardsToMatch(all_cards, class_to_match) {
-
-    // looks for any string between 'images/' and '.gif'
-    const extractGifRegex = /images\/(.*).gif/;
-
-    let cardsToTest = [];    
-
-    for(let card in all_cards) {
-        // executes the regex rule in the <img> src, extracting the gif file name
-        const cardClass = extractGifRegex.exec(all_cards[card].childNodes[1].src)[1];
-        if(cardClass === class_to_match) {
-            cardsToTest.push(all_cards[card]);
-        }
-    }    
-    return cardsToTest;
-}
-
 // asks the total number of cards to de displayed in the screen
 function askCardQuantity() {
     let cardQuantity = 0;
@@ -225,7 +17,6 @@ function askCardQuantity() {
     while(cardQuantity < 4 || cardQuantity > 14 || (cardQuantity % 2 !== 0)) {
         cardQuantity = prompt('Por favor, insira o número de cartas que deseja:');
     }
-
     createGameCards(cardQuantity);
 }
 
@@ -254,11 +45,11 @@ function createGameCards(cardQuantity) {
     }
 
     // adds the cards in the HTML, the number of cards added is the 'cardQuantity'
-    for(let contador = 0; contador < cardQuantity; contador++) {
+    for(let cardCounter = 0; cardCounter < cardQuantity; cardCounter++) {
         cardSection.innerHTML += `
         <div class="card" onclick="testCard(this)">
             <div class="cardFront rotate">
-                <img src="images/${cardClasses[contador]}.gif">
+                <img src="images/${cardClasses[cardCounter]}.gif">
             </div>
             <div class="cardBack">
                 <img src="images/back.png">
@@ -268,7 +59,7 @@ function createGameCards(cardQuantity) {
     }
 }
 
-function shuffleCards(card_classes_to_shuffle) {
+function shuffleCards(cardClassesToShuffle) {
     /*
         ------ Example:
         originalCardsClassesIndexes = [0, 1, 2, ...]
@@ -282,7 +73,7 @@ function shuffleCards(card_classes_to_shuffle) {
 
    // creates a copy of the array received as a parameter
     const originalCardsClassesIndexes = [];
-    const originalCardsClasses = card_classes_to_shuffle.slice()
+    const originalCardsClasses = cardClassesToShuffle.slice()
    
     let shuffledCardsClassesIndexes = [];
     let shuffledCardsClasses = [];
@@ -309,4 +100,208 @@ function shuffleCards(card_classes_to_shuffle) {
 function doubleTheArray(arrayToDouble) {
     arrayToDouble = arrayToDouble.concat(arrayToDouble);
     return arrayToDouble;
+}
+
+// called everytime the user clicks card
+function testCard(card) {
+    
+    /* 
+        In this game every card has a div for it's front part and a div for 
+        it's back part, we must select both to create our flipping cards logic 
+    */
+    const cardsFront = Array.from(document.querySelectorAll('.cardFront'));
+    const cardsBack = Array.from(document.querySelectorAll('.cardBack'));
+
+    /*
+        childNodes[3] -> selects div.cardBack
+        childNodes[1] -> selects div.cardFront
+    */
+    let clickedCardBack = card.childNodes[3];
+    let clickedCardFront = card.childNodes[1];          
+    
+    // variable containing the classe name of the card clicked
+    const cardClass = extractNameFromGifFile(clickedCardFront);
+    
+    // extracts an array containing only the 2 cards with the class being tested
+    const cardsToMatch = extractCardsToMatch(cardsFront, cardClass);    
+
+    // the game does not flip a matched card
+    if(clickedCardBack.classList.contains('rotate') && cardIsMatched(getFlippedCards(cardsFront,cardsBack)[0], clickedCardFront)) {
+        return;
+    }
+    
+    // flips the card clicked
+    flipCard(clickedCardBack, clickedCardFront);
+
+    // checks if the card clicked is matched
+    if(testForMatch(cardsToMatch)) {
+        numberOfPlays++;              
+        changeMatchedCardsColor(cardsFront);
+    } else {
+        flipBothCardsDisplayed(cardsFront, cardsBack);
+    }
+
+    // checks if the game is over, if it is displays the 'numberOfPlays' played
+    if(gameIsOver(cardsFront)) {
+        setTimeout(function() {
+            alert(`Você ganhou em ${numberOfPlays} jogadas!`);       
+        }, 500);
+    }
+}
+
+// extracts the name class from the gif img file (uses regex)
+function extractNameFromGifFile(cardFrontDiv) {
+
+    // regex user for extracting the gif name from the image file
+    const extractGifRegex = /images\/(.*).gif/;
+
+    // applies the regex above in the card front div received
+    const gifFileName = extractGifRegex.exec(cardFrontDiv.childNodes[1].src)[1];
+
+    return gifFileName;
+}
+
+// extracts an array containing only the 2 cards with the class being tested
+function extractCardsToMatch(allCardsFrontDiv, classToMatch) {
+
+    let cardsToMatch = [];    
+
+    for(let card in allCardsFrontDiv) {
+        // executes the regex rule in the <img> src, extracting the gif file name
+        const cardClass = extractNameFromGifFile(allCardsFrontDiv[card]);
+        if(cardClass === classToMatch) {
+            cardsToMatch.push(allCardsFrontDiv[card]);
+        }
+    }    
+    return cardsToMatch;
+}
+
+function cardIsMatched(cardsFlipedFrontDiv, cardToAnalyse) {
+
+    // variable containing the classe name of the card clicked
+    const cardTestedClass = extractNameFromGifFile(cardToAnalyse)
+    const cardsToAnalyzeClasses = [];
+    const classesWithBothCardsFlipped = [];
+
+    for(let cards in cardsFlipedFrontDiv) {
+        cardsToAnalyzeClasses.push(extractNameFromGifFile(cardsFlipedFrontDiv[cards]));
+    }
+
+    for(let currentCard = 0; currentCard < cardsToAnalyzeClasses.length; currentCard++) {
+        for(let nextCard = currentCard + 1; nextCard < cardsToAnalyzeClasses.length; nextCard++) {
+            if(cardsToAnalyzeClasses[currentCard] === cardsToAnalyzeClasses[nextCard]) {
+                classesWithBothCardsFlipped.push(cardsToAnalyzeClasses[currentCard]);
+            }
+        }
+    }
+
+    if(classesWithBothCardsFlipped.includes(cardTestedClass)) {
+        return true;
+    }
+    return false;
+}
+
+// returns an matrix containing all the cards flipped front and back divs
+function getFlippedCards(allCardsFrontDiv, allCardsBackDiv){
+    // arrays containing all cards fliped front and back div
+    const cardsFlipedFrontDiv = [];
+    const cardsFlipedBackDiv = [];
+    
+    for(let cardCounter in allCardsFrontDiv) {
+        if(!allCardsFrontDiv[cardCounter].classList.contains('rotate')) {
+            cardsFlipedFrontDiv.push(allCardsFrontDiv[cardCounter]);
+            cardsFlipedBackDiv.push(allCardsBackDiv[cardCounter]);
+        }
+    }    
+    return [cardsFlipedFrontDiv, cardsFlipedBackDiv];
+}
+
+// flips the card switching the '.rotate' class from both card "sub divs" (cardBack and cardFront)
+function flipCard(cardBack, cardFront) {
+    cardFront.classList.toggle('rotate');
+    cardBack.classList.toggle('rotate');
+}
+
+// checks if there was a match for the card selected
+function testForMatch(cardsToTest) {
+    if(numberOfCardsHidden(cardsToTest) === 0) {
+        return true;
+    }
+    return false;
+}
+
+// checks the quantity of hidden cards in a certain array, returns the number of cards hidden
+function numberOfCardsHidden(cardsToTest){
+    let cardsHidden = 0;
+
+    for(let card in cardsToTest){
+        if(cardsToTest[card].classList.contains('rotate')) {
+            cardsHidden++
+        }
+    }
+    return cardsHidden;
+}
+
+// adds pink background color to card when it's match is found
+function changeMatchedCardsColor(cardsFrontDiv) {
+    for(let cardCounter in cardsFrontDiv) {
+        if(!cardsFrontDiv[cardCounter].classList.contains('rotate')) {
+            cardsFrontDiv[cardCounter].classList.add('cardMatched');
+        }
+    }
+}
+
+// if the 2 cards selected were not matched, the game must flips both cards back
+function flipBothCardsDisplayed(allCardsFront, allCardsBack) {
+    // arrays containing all cards fliped front and back div
+    const cardsFlipedFront = getFlippedCards(allCardsFront, allCardsBack)[0];
+    const cardsFlipedBack = getFlippedCards(allCardsFront, allCardsBack)[1];
+
+    let quantityOfCardsFliped = 0;
+
+    setTimeout(() => {
+        for(let cardCounter in cardsFlipedFront) {
+            if(!cardIsMatched(cardsFlipedFront, cardsFlipedFront[cardCounter]) && !cardIsAlone(cardsFlipedFront)) {
+                flipCard(cardsFlipedFront[cardCounter], cardsFlipedBack[cardCounter]);
+                quantityOfCardsFliped++;
+            }
+        }  
+        // checks if the play was completed, so we can increment 'numberOfPlays'
+        if(quantityOfCardsFliped >= 2) {
+            numberOfPlays++;
+        }
+    }, 600);
+}
+
+// checks if just 1 card was selected, so we can mantain it being displayed
+function cardIsAlone(cardsFliped) {
+
+    // this variable will be decremented so we can disconsider the cards that were already match
+    let quantityOfCardsFlipped = cardsFliped.length;    
+
+   // variable containing the classe name of the card clicked
+   const cardsToAnalyzeClasses = [];
+
+   for(let cards in cardsFliped) {
+       cardsToAnalyzeClasses.push(extractNameFromGifFile(cardsFliped[cards]));
+   }
+
+   // looks for cards that were matched decrementing de 'quantityOfCardsFlipped'
+   for(let currentCard = 0; currentCard < cardsToAnalyzeClasses.length; currentCard++) {
+       for(let nextCard = currentCard + 1; nextCard < cardsToAnalyzeClasses.length; nextCard++) {
+           if(cardsToAnalyzeClasses[currentCard] === cardsToAnalyzeClasses[nextCard]) {
+            quantityOfCardsFlipped -= 2;
+           }
+       }
+   }
+
+   return (quantityOfCardsFlipped === 1);
+}
+
+// checks if all cards were flipped
+function gameIsOver(cardsFront) {
+    if(numberOfCardsHidden(cardsFront) === 0) {
+        return true;
+    }
+    return false;
 }
